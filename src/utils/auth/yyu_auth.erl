6b0187ec70c5747db1eb5c0ac1ea293f02992ca3
隐叶 <106641632+yinye1024@users.noreply.github.com>
2024-03-20 20:13:56 +0800

    add new method

--- src/utils/auth/yyu_auth.erl
+++ src/utils/auth/yyu_auth.erl
@@ -11,7 +11,7 @@
 
 
 %% API functions defined
--export([to_md5/1,to_lowercase_md5/1,to_upercase_md5/1]).
+-export([to_md5/1,to_lowercase_md5/1,to_upercase_md5/1,build_time_ticket/3,check_time_ticket/4]).
 
 
 %% ===================================================================================
@@ -27,3 +27,13 @@ to_lowercase_md5(InputStr) ->
 to_upercase_md5(InputStr) ->
   <<N:128>> = to_md5(InputStr),
   lists:flatten(io_lib:format("~32.16.0B",[N])).
+
+
+build_time_ticket(UserId,Time,Key)->
+  Md5Str = [yyu_misc:to_list(UserId),yyu_misc:to_list(Time),yyu_misc:to_list(Key)],
+  Ticket = to_lowercase_md5(Md5Str),
+  Ticket.
+
+check_time_ticket(UserId,Time,Key,TicketP)->
+  TicketP == build_time_ticket(UserId,Time,Key).
+
